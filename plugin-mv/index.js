707109c1175d260afadc80327e7cc14443c55693
miaoyu <miaoyu2009@qq.com>
2021-05-18 17:31:51 +0800

    chore: change path

--- plugin-mv/index.js
+++ plugin-mv/index.js
@@ -1,26 +1,26 @@
-const fs = require('fs-extra')
-const path = require('path')
+const fs = require("fs-extra");
+const path = require("path");
 
 export default (ctx, options) => {
   ctx.onBuildFinish(() => {
-   
     // Taro v3.1.4
-    const blended = ctx.runOpts.blended || ctx.runOpts.options.blended
-    
-    if (!blended) return
+    const blended = ctx.runOpts.blended || ctx.runOpts.options.blended;
 
-    console.log('编译结束！')
+    if (blended) {
+      console.log("小程序编译结束！");
 
-    const rootPath = path.resolve(__dirname, '../..')
-    const miniappPath = path.join(rootPath, 'miniapp')
-    const outputPath = path.resolve(__dirname, '../dist')
-    const destPath = path.join(miniappPath, 'taro')
+      const rootPath = path.resolve(__dirname, "../..");
+      const miniappPath = path.join(rootPath, "src");
+      const outputPath = path.resolve(__dirname, "../dist");
+      const destPath = path.join(miniappPath, "taro");
 
-    if (fs.existsSync(destPath)) {
-      fs.removeSync(destPath)
+      if (fs.existsSync(destPath)) {
+        fs.removeSync(destPath);
+      }
+      fs.copySync(outputPath, destPath);
+
+      console.log("编译代码拷贝结束！");
+      return;
     }
-    fs.copySync(outputPath, destPath)
-
-    console.log('拷贝结束！')
-  })
-}
+  });
+};
