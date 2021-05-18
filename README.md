a715711be170028055a75f253567e536bd23bf54
miaoyu <miaoyu2009@qq.com>
2021-05-18 14:17:21 +0800

    init 🎉

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-## 在原生小程序项目中使用 Taro
-
-> Taro `3.0.25+`
-
-### Start
-
-1. 编译
-
-```bash
-cd taro-project
-taro build --type weapp --blended
-```
-
-2. 开发者工具打开 `miniapp` 项目
+### shop sales wx app next 
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import { printCommon } from '../../common'
-import { logRaw } from '@/utils/util'
+// import { logRaw } from '@/utils/util'
 import './index.scss'
 
 export default class Index extends Component {
   componentDidMount () {
     printCommon()
-    logRaw()
+    // logRaw()
   }
 
   render () {
