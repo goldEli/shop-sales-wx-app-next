678589993543a713c7b4fd208dc91f15320c15dc
miaoyu <miaoyu2009@qq.com>
2021-05-20 11:33:18 +0800

    chore: rollup mutiple inputs

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,8 @@ import RollupJson from "@rollup/plugin-json";
 import RollupNodeResolve from "@rollup/plugin-node-resolve";
 import RollupCommonjs from "@rollup/plugin-commonjs";
 import RollupTypescript from "rollup-plugin-typescript2";
+import multiInput from "rollup-plugin-multi-input";
+// const copyFile = require("./plugin");
 
 const resolveFile = (path) => NodePath.resolve(__dirname, path);
 const externalPackages = [
@@ -15,9 +17,60 @@ const externalPackages = [
   "@tarojs/react",
 ];
 
-const basicConfig = {
+// const basicConfig = {
+//   input: resolveFile("./src/pages/home/index.tsx"),
+//   output: {
+//     file: "dist/pages/home/index.js",
+//     format: "esm",
+//     sourcemap: true,
+//   },
+//   //   external: externalPackages,
+//   plugins: [
+//     RollupCommonjs({
+//       include: /\/node_modules\//,
+//     }),
+//     RollupJson(),
+//     // nodeResolve(),
+//     scss(),
+//     RollupTypescript({
+//       tsconfig: resolveFile("./tsconfig.json"),
+//       check: false,
+//     }),
+//   ],
+// };
+
+// const inputAndOutputs = [
+//   {
+//     input: resolveFile("./src/pages/home/index.tsx"),
+//     output: {
+//       file: "dist/pages/home/index.js",
+//       format: "esm",
+//       sourcemap: true,
+//     },
+//   },
+//   {
+//     input: resolveFile("./src/pages/index/index.tsx"),
+//     output: {
+//       file: "dist/pages/index/index.js",
+//       format: "esm",
+//       sourcemap: true,
+//     },
+//   },
+// ];
+
+export default {
+  input: [
+    resolveFile("./src/pages/home/index.tsx"),
+    resolveFile("./src/pages/index/index.tsx"),
+  ],
+  output: {
+    dir: "dist",
+    format: "esm",
+    sourcemap: true,
+  },
   //   external: externalPackages,
   plugins: [
+    multiInput(),
     RollupCommonjs({
       include: /\/node_modules\//,
     }),
@@ -30,24 +83,3 @@ const basicConfig = {
     }),
   ],
 };
-
-const inputAndOutputs = [
-  {
-    input: resolveFile("./src/pages/home/index.tsx"),
-    output: {
-      file: "dist/pages/home/index.js",
-      format: "esm",
-      sourcemap: true,
-    },
-  },
-  {
-    input: resolveFile("./src/pages/index/index.tsx"),
-    output: {
-      file: "dist/pages/index/index.js",
-      format: "esm",
-      sourcemap: true,
-    },
-  },
-];
-
-export default inputAndOutputs.map((item) => ({ ...item, ...basicConfig }));
